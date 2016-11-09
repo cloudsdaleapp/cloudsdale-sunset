c86c4160dc567e35884ee21604d2e7f7ee3ed507
Tyrone Trevorrow <tyrone@sudeium.com>
2016-11-09 22:47:25 +1100

    Fixes 404, adds FAQ, fixes Windows font

--- app.js
+++ app.js
@@ -27,10 +27,7 @@ app.use('/', sessions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    // var err = new Error('Not Found');
-    // err.status = 404;
-    // next(err);
-    res.render('index', { title: 'Coming soon!' });
+    res.redirect("/");
 });
 
 // error handlers
